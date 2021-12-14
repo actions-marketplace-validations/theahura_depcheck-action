4a77bfbf8dd730413844755482f2a53927da7b2c
Amol Kapoor <theahura@gmail.com>
2021-12-14 17:10:50 -0500

    Add some more debug print info

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Checking deps for $GITHUB_REPOSITORY"
+echo "Checking deps for $GITHUB_REPOSITORY from $GITHUB_WORKSPACE"
+sh -c "depcheck --version"
 sh -c "depcheck $GITHUB_WORKSPACE $*"
